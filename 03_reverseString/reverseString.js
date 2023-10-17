dd707174d066c3585f34152a246aaf690e78ad0a
Shadman <sh4dman0001@gmail.com>
2023-10-17 15:43:53 +0600

    Finished 3rd excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Return undefined if no string is provided or it is undefined
+    if (string == null) {
+        return undefined;
+    }
 
+    let result = '';
+    string = String(string);
+    for (let i = string.length - 1; i >= 0; i--) {
+        result += string.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
