b011de7e2ee138000c2e4ee413a3663fbeb3008c
Shadman <sh4dman0001@gmail.com>
2023-10-17 18:26:56 +0600

    Minor changes

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year == null || typeof year !== 'number') {
+        return 'ERROR';
+    }
 
+    return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
 };
 
 // Do not edit below this line
