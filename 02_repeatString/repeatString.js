e42bc355d0414054c02c13b4d42dadce571c8f89
Shadman <sh4dman0001@gmail.com>
2023-10-17 15:20:34 +0600

    Finish 2nd excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, timesToRepeat) {
+    if (timesToRepeat < 0 || Math.floor(timesToRepeat) !== timesToRepeat) {
+        return 'ERROR';
+    }
+    let result = '';
+    for (let i = 0; i < timesToRepeat; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
