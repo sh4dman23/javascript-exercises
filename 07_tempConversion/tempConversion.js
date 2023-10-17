94a49f309038d7da88e46d75860c757a6a1128aa
Shadman <sh4dman0001@gmail.com>
2023-10-17 18:24:38 +0600

    Finish 7th excercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInF) {
+    if (temperatureInF == null || typeof temperatureInF !== 'number') {
+        return 'ERROR';
+    }
+
+    const temperatureInC = (temperatureInF - 32) * 5 / 9;
+
+    return Math.floor(temperatureInC) === temperatureInC ? parseInt(temperatureInC) : Math.round(temperatureInC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInC) {
+    if (temperatureInC == null || typeof temperatureInC !== 'number') {
+        return 'ERROR';
+    }
+
+    const temperatureInF = temperatureInC * 9 / 5 + 32;
+
+    return Math.floor(temperatureInF) === temperatureInF ? parseInt(temperatureInF) : Math.round(temperatureInF * 10) / 10;
 };
 
 // Do not edit below this line
