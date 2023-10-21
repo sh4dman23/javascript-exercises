bd4b46f9fc34b4ab0d7662b5b7eec4b0c18560e4
Shadman <sh4dman0001@gmail.com>
2023-10-21 12:57:58 +0600

    Complete 10th excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (isNaN(num) || num < 0) {
+        return 'OOPS';
+    } else if (typeof num === 'string') {
+        num = Number(num);
+    }
 
+    // Default cases
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
