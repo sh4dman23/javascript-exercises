8b5b9e132e287ef52c77b3ad24fca0bc72336898
Shadman <sh4dman0001@gmail.com>
2023-10-21 13:44:42 +0600

    Complete 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfPeople) {
+    if (arrayOfPeople == null) {
+        return undefined;
+    }
 
+    const currentYear = (new Date()).getFullYear();
+
+    return arrayOfPeople.reduce((oldestPerson, person) => {
+        const newPersonIsOlder = (person.yearOfDeath ? person.yearOfDeath : currentYear) - person.yearOfBirth >
+                        (oldestPerson.yearOfDeath ? oldestPerson.yearOfDeath : currentYear) - oldestPerson.yearOfBirth;
+
+        return newPersonIsOlder ? person : oldestPerson;
+    }
+    , arrayOfPeople[0]);
 };
 
 // Do not edit below this line
