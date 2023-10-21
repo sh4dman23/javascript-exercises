bed40ed674d0f093c5a5a5531901dc6208f4f36b
Shadman <sh4dman0001@gmail.com>
2023-10-21 12:45:46 +0600

    Complete 8th & 9th excercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,61 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    if (!Array.isArray(array)) {
+        return undefined;
+    } else if (array.length === 0) {
+        return 0;
+    }
+
+    return array.reduce((sumOfElements, element) => {
+        if (!isNaN(element)) {
+            sumOfElements += Number(element);
+        }
+        return sumOfElements;
+    });
 };
 
-const multiply = function() {
+const multiply = function(array) {
+    if (!Array.isArray(array)) {
+        return undefined;
+    } else if (array.length === 0) {
+        return 0;
+    }
 
+    return array.reduce((productOfElements, element) => {
+        if (!isNaN(element)) {
+            productOfElements *= element;
+        }
+        return productOfElements;
+    });
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+    return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num < 0 || Math.floor(num) !== num || isNaN(num)) {
+        return undefined;
+    } else if (num === 0) {
+        return 1;
+    }
+
+    return num * factorial(num - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const punctuations = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
+    const chars = string.toLowerCase().split('');
+
+    const charsWithoutPunctuation = chars.filter(char => !(punctuations + ' ').includes(char));
+
+    // String without punctuations === reversed string without punctuations?
+    return charsWithoutPunctuation.join('') === charsWithoutPunctuation.reverse().join('');
 };
 
 // Do not edit below this line
